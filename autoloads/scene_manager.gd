c209ba589d153417c8c6a77aadecc1a52072947a
ChrstphrHll <thchrstphrhll@gmail.com>
2026-08-23 17:49:14 -0400

    scene manager

---
+++ autoloads/scene_manager.gd
@@ -0,0 +1,22 @@
+extends Node
+
+
+var SCENE_PATHS: Dictionary = {
+	"main_menu": "res://menu/menu.tscn",
+	"testing_scene": "res://testing_scene.tscn"
+}
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print("scene manager ready")
+
+
+func go_to(scene_id: String) -> Error:
+	var scene_path: String = SCENE_PATHS.get(scene_id, "")
+	if scene_path.is_empty():
+		push_error("Unknown scene ID: %s", scene_id)
+		return ERR_DOES_NOT_EXIST
+	
+	get_tree().paused = false
+	return get_tree().change_scene_to_file(scene_path)
---
+++ menu/menu.gd
@@ -0,0 +1,8 @@
+extends CenterContainer
+
+@onready var play_button = $PlayButton
+
+
+
+func _on_play_button_pressed():
+	SceneManager.go_to("testing_scene")
