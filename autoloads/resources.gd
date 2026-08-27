610fc55d2dd32939f86b9f0aa7d09b51b269aadb
JohnETobin <johntobin788@gmail.com>
2026-08-26 20:44:06 -0400

    Add visible resource tracker

--- autoloads/resources.gd
+++ autoloads/resources.gd
@@ -1,23 +1,53 @@
 extends Node
 
+signal resource_changed(resource_name: String, new_amount: int)
+
+
 var _resources = {
 	"wood": {
 		"amount": 0,
 		"difficulty": 1,
-		"abundance": 1
+		"abundance": 1,
+		"icon": preload("res://assets/WoodResource.png")
 	}
 }
 
 
 func modify_resource(resource: String, delta: int):
-	var current_amount = _resources.get(resource).amount
-	_resources[resource].amount = current_amount + delta
-	print(_resources[resource].amount)
+	if not _resources.has(resource):
+		push_warning("Tried to modify unknown resource: " + resource)
+		return
+
+	_resources[resource]["amount"] += delta
+
+	resource_changed.emit(
+		resource,
+		_resources[resource]["amount"]
+	)
 
 
-func get_resource_abundance(resource):
-	return _resources[resource].abundance
+func get_resource_amount(resource: String) -> int:
+	if not _resources.has(resource):
+		push_warning("Tried to get unknown resource: " + resource)
+		return 0
+
+	return _resources[resource]["amount"]
 
 
-func get_resource_difficulty(resource):
-	return _resources[resource].difficulty
+func get_resource_names():
+	return _resources.keys()
+
+
+func get_resource_icon(resource: String):
+	if not _resources.has(resource):
+		return null
+
+	return _resources[resource].get("icon", null)
+
+
+func get_resource_abundance(resource: String):
+	return _resources[resource]["abundance"]
+
+
+func get_resource_difficulty(resource: String):
+	return _resources[resource]["difficulty"]
---
+++ resource_display.gd
@@ -0,0 +1,69 @@
+extends HBoxContainer
+
+var resource_entries: Dictionary = {}
+
+
+func _ready():
+	Resources.resource_changed.connect(_on_resource_changed)
+
+	for resource_name in Resources.get_resource_names():
+		create_resource_entry(resource_name)
+
+
+func create_resource_entry(resource_name: String):
+	# prevent duplicate resources
+	if resource_entries.has(resource_name):
+		return
+
+	# Each resource gets one vertical column.
+	var column = VBoxContainer.new()
+	add_child(column)
+
+	# Optional icon
+	var icon = TextureRect.new()
+
+	icon.custom_minimum_size = Vector2(48, 48)
+	icon.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
+	icon.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_CENTERED
+
+	column.add_child(icon)
+
+	var icon_texture = Resources.get_resource_icon(resource_name)
+	icon.texture = icon_texture
+	icon.visible = icon_texture != null
+
+	# Resource name
+	var name_label = Label.new()
+	name_label.text = resource_name.capitalize()
+	column.add_child(name_label)
+
+	# Resource amount
+	var amount_label = Label.new()
+	column.add_child(amount_label)
+
+	# Remember all the UI objects associated with this resource
+	resource_entries[resource_name] = {
+		"column": column,
+		"icon": icon,
+		"name_label": name_label,
+		"amount_label": amount_label
+	}
+
+	update_resource_entry(
+		resource_name,
+		Resources.get_resource_amount(resource_name)
+	)
+
+
+func _on_resource_changed(resource_name: String, new_amount: int):
+	# allows code to work if a resource is introduced
+	# after the UI has already loaded
+	if not resource_entries.has(resource_name):
+		create_resource_entry(resource_name)
+
+	update_resource_entry(resource_name, new_amount)
+
+
+func update_resource_entry(resource_name: String, amount: int):
+	var entry = resource_entries[resource_name]
+	entry["amount_label"].text = str(amount)
